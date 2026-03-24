bef96eac4cbf6196945d53eea5df09b7fd3b7f40
JONATAN CARDENAS <cardenasjonatan7@gmail.com>
2026-03-24 15:26:50 -0500

    comentarios  del proceso

--- src/ArbolInventario.java
+++ src/ArbolInventario.java
@@ -3,16 +3,17 @@ public class ArbolInventario {
     int tamaño;
 
     public ArbolInventario() {
+        //INICIALIZAR EL ARBOL
        raiz = null;
        tamaño =0;
     }
     
     private String inOrden(Producto producto){
+        //verificar que el producto no sea nulo
         if (producto!=null) {
                //1.recorrer el subarbol izquierdo
          return inOrden(producto.izquierda) + producto.id + " - " + producto.nombre + ", " + inOrden(producto.derecho);
               //2.procesar el dato
-           
               //3. Recorrer el subarbol derecho
         }
         return "";
@@ -61,18 +62,21 @@ public class ArbolInventario {
     
     
     public void insertar(int id, String nombre){
+        //VERIFICAR SI EL ARBOL ESTA VACIO
         if (raiz==null) {
-
+//CREAR EL NUEVO NODO Y ASIGNARLO COMO RAIZ
             raiz=new Producto(id, nombre);
             tamaño++;
         }else{
+            //CREAR EL NUEVO NODO
             Producto aux= raiz;
             boolean bandera=true;
-            while (bandera) {                
+            while (bandera) {   
+                //VERIFICAR SI EL ID ES MENOR O MAYOR QUE EL ID DEL NODO ACTUAL             
                 if (id < aux.id) {
                     if (aux.izquierda!=null) {
                         aux=aux.izquierda;
-                        
+                        //SI EL NODO IZQUIERDO NO ES NULO, MOVERSE A ESE NODO
                     }else{
                       aux.izquierda=new Producto(id, nombre);
                       tamaño++;
@@ -80,12 +84,14 @@ public class ArbolInventario {
                     
                     }
                     
-                    
+                    //SI EL NODO IZQUIERDO ES NULO, INSERTAR EL NUEVO NODO A LA IZQUIERDA DEL NODO ACTUAL Y TERMINAR EL PROCESO 
                 }else{
                 if (aux.derecho!=null) {
+                    //SI EL NODO DERECHO NO ES NULO, MOVERSE A ESE NODO
                         aux=aux.derecho;
                         
                     }else{
+                        //SI EL NODO DERECHO ES NULO, INSERTAR EL NUEVO NODO A LA DERECHA DEL NODO ACTUAL Y TERMINAR EL PROCESO
                       aux.derecho=new Producto(id, nombre);
                       tamaño++;
                       bandera=false;
@@ -99,15 +105,22 @@ public class ArbolInventario {
           
      }
     public boolean buscar(int dato){
+        //CREAR UN NODO AUXILIAR PARA RECORRER EL ARBOL
         Producto aux=raiz;
+        //RECORRER EL ARBOL HASTA ENCONTRAR EL DATO O LLEGAR A UN NODO NULO
         while (aux!=null) {  
+            //VERIFICAR SI EL DATO ES IGUAL AL ID DEL NODO ACTUAL       
             if (aux.id==dato) {
+                //SI EL DATO ES IGUAL AL ID DEL NODO ACTUAL, RETORNAR TRUE
                 return true;
             }else{
+                //SI EL DATO ES MENOR QUE EL ID DEL NODO ACTUAL, MOVERSE AL NODO IZQUIERDO, SI EL DATO ES MAYOR QUE EL ID DEL NODO ACTUAL, MOVERSE AL NODO DERECHO
                 if (dato<aux.id) {
+                    //SI EL DATO ES MENOR QUE EL ID DEL NODO ACTUAL, MOVERSE AL NODO IZQUIERDO
                     aux=aux.izquierda;
                     
                 }else{
+                    //SI EL DATO ES MAYOR QUE EL ID DEL NODO ACTUAL, MOVERSE AL NODO DERECHO
                     aux=aux.derecho;
                 
                 
@@ -117,7 +130,7 @@ public class ArbolInventario {
             
         
     
-    }          
+    }          //SI SE LLEGA A UN NODO NULO, SIGNIFICA QUE EL DATO NO SE ENCONTRO EN EL ARBOL, RETORNAR FALSE
      return  false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
      try (Scanner teclado = new Scanner(System.in)) {
+        //CREAR EL ARBOL
         ArbolInventario arbol=new ArbolInventario();
          int dato = 0;
+         //VARIABLE PARA GUARDAR LA OPCION DEL USUARIO
             String opcion="";
          
            do {              
-                 menu();
+                 menu();// mostrar el menu
                  System.out.println("INGRESE UNA OPCION: ");
+                 //LEER LA OPCION DEL USUARIO
                  opcion=teclado.nextLine();
                    switch (opcion) {
                 case "1":
@@ -17,11 +20,17 @@ public class Main {
                      while (true) {
                          System.out.println("***REGISTRO***\n");
                          System.out.println("INGRESE EL ID DEL PRODUCTO: ");
+                         //LEER EL DATO DEL USUARIO
                             dato = teclado.nextInt();
+                            //LIMPIAR EL BUFFER
                             teclado.nextLine();
+                           
                             System.out.println("INGRESE EL NOMBRE DEL PRODUCTO: ");
+                            //LEER EL NOMBRE DEL PRODUCTO
                             String nombre = teclado.nextLine();
+                            //INSERTAR EL DATO EN EL ARBOL
                             arbol.insertar(dato, nombre);
+                            
                            System.out.println("DESEA REGISTRAR OTRO DATO? \n 1.SI 2.NO");
                          opcion=teclado.nextLine();
                          switch (opcion) {
@@ -38,13 +47,16 @@ public class Main {
                     break;
                     case "2":
                       System.out.println("***MOSTRAR INVENTARIO (INORDEN)***\n");
+                      //MOSTRAR EL INVENTARIO EN INORDEN
                       System.out.println(arbol.inOrden());
                     break;
 
                     case "3":
                             System.out.println("***INGRESE EL DATO QUE DESEA BUSCAR***\n");
+                            //LEER EL DATO DEL USUARIO
                             dato = teclado.nextInt();
                             teclado.nextLine();
+                            //BUSCAR EL DATO EN EL ARBOL
                             if(arbol.buscar(dato)){
                                 System.out.println("DATO ENCONTRADO");
                             }else{
@@ -54,10 +66,12 @@ public class Main {
                 
                 case "4":
                     System.out.println("***MOSTRAR POSTORDEN***\n");
+                    //MOSTRAR EL INVENTARIO EN POSTORDEN
                       arbol.postOrden();
                     break;
                 case "5":
                    System.out.println("***MOSTRAR PREORDEN***\n");
+                   //MOSTRAR EL INVENTARIO EN PREORDEN
                       arbol.preOrden();
                       break;
                 case "6":
@@ -70,7 +84,7 @@ public class Main {
      System.out.println("PROGRAMA TERMINADO...");
        
     }
-    
+    //METODO PARA MOSTRAR EL MENU
   public static void menu(){
       System.out.println("=====ARBOL BINARIO=====");
       System.out.println("1.INGRESAR DATO");
